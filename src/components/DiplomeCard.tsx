64459f2dce85051531850e9b99f7b5727664b53c
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2023-02-13 23:43:32 +0000

    gerer la responsivite du card diplome

--- src/components/DiplomeCard.tsx
+++ src/components/DiplomeCard.tsx
@@ -4,10 +4,12 @@ import bakeli from '../assets/img/bakeli.png'
 
 const DiplomeCard = () => {
     return (
-        <div style={{backgroundColor:'white', boxShadow: '0px 0px 5px rgba(10, 14, 16, 0.15)', borderRadius: '15px'}} className='w-100 flex justify-content-between p-2 mt-2'>
-            <img style={{ minWidth: '200px', objectFit: 'cover' }} src={bakeli} alt="Bakeli" />
+        <div className='diplome-card w-100 flex gap-2 p-2 mt-2'>
+            <div className='diplome-img--container text-700-center'>
+                <img className='diplome-card--img' src={bakeli} alt="Bakeli" />
+            </div>
             <div>
-                <h3 className='flex justify-content-between align-items-center gap-2'>
+                <h3 className='flex justify-content-between align-items-center gap-2 m-0'>
                     <span>BTS Marketing Digital et communication</span>
                     <span style={{
                         display: 'inline-block',
@@ -19,7 +21,7 @@ const DiplomeCard = () => {
                     }}
                     >Bac</span>
                 </h3>
-                <p>ANAQ-SUP</p>
+                <p className='m-0'>ANAQ-SUP</p>
                 <p className='lieu flex gap-2 align-items-center'>
                     <span className='flex align-items-center' ><LocationCityOutlined /> Thies</span>
                     <span style={{
@@ -31,12 +33,12 @@ const DiplomeCard = () => {
                     }}>Presentiel</span>
                 </p>
             </div>
-            <div className="date">
-                <p>Jan 2023</p>
-                <p>A votre rythme</p>
+            <div className="date m-0">
+                <p className='m-0'>Jan 2023</p>
+                <p className='m'>A votre rythme</p>
             </div>
-            <div className="actions">
-                <button className='btn'>Modifier</button>
+            <div className="diplome-actions">
+                <button className='btn btn-secondary btn-100'>Modifier</button>
             </div>
         </div>
     )
